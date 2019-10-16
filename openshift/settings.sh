ce551a19ec760a1e106ea47dc7aa24798d58d2f3
Bonito <mario.bonito@prod.prv>
2019-10-16 11:17:47 -0400

    added gc specifc configuration

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,15 +1,21 @@
-export PROJECT_NAMESPACE="devex-von"
-export PROJECT_OS_DIR=${PROJECT_OS_DIR:-../../openshift}
+export PROJECT_NAMESPACE="gc-orgbook"
+export PROJECT_OS_DIR="../../openshift"
 
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
 # with the values of GIT_URI and GIT_REF
 export skip_git_overrides="schema-spy-build.json solr-base-build.json backup-build.json"
-export GIT_URI="https://github.com/bcgov/TheOrgBook.git"
-export GIT_REF="master"
+export GIT_URI="https://github.com/ised-isde-canada/TheOrgBook"
+export GIT_REF="develop"
+
+export TOOLS="gc-orgbook-dev"
+export DEPLOYMENT_ENV_NAME="dev"
+export DEV="dev"
+export TEST="dev"
+export PROD="dev"
 
 # The project components
-export components="tob-db tob-solr tob-api tob-web tob-wallet tob-backup"
+# export components="tob-db tob-solr tob-api tob-web tob-wallet tob-backup" # - removed due to loop bug
 
 # The builds to be triggered after buildconfigs created (not auto-triggered)
 export builds=""
